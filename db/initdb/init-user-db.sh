27a478b639bccf5ab6f21fe747218634b58b636a
Lachlan Kingsford <lachlan@nerdygentleman.com>
2020-09-20 13:54:48 +1000

    Testing dockerfile setup

---
+++ db/initdb/init-user-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE users (
+        user_id SERIAL,
+        user_name TEXT,
+        pass_hash TEXT,
+        user_email TEXT
+    );
+EOSQL
