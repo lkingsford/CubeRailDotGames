f6a81aeafb4599903ad855305720628f4a88a651
Lachlan Kingsford <lachlan@nerdygentleman.com>
2020-11-27 23:44:03 +0000

    Koa test

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,14 @@
 const { Server } = require('boardgame.io/server')
 const { TicTacToe } = require('../game/game')
 import { PostgresStore } from "bgio-postgres"
+import * as Koa from 'koa';
 
 const db = new PostgresStore(process.env['DB']!);
 const server = Server({ games: [TicTacToe], db});
+
 server.run(3500)
+
+server.router.get("/hello" , async (ctx:Koa.Context) =>
+{
+    ctx.body = "ROCK YA BODY";
+});
