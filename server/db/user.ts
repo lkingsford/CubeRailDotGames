b0da46e9b3bedd1998d356f5d2c69929ee018f81
Lachlan Kingsford <lachlan@nerdygentleman.com>
2021-03-03 03:20:28 +0000

    Create account and save session to DB

--- server/db/user.ts
+++ server/db/user.ts
@@ -1,48 +1,120 @@
 import { pool } from "./db";
+import { getHash, passwordOk } from '../auth';
 
-export interface IUser {
-    userId?: number;
-    username: string;
-    passwordHash: string;
-    role: string;
+export enum UserCreateResult {
+    success,
+    badPassword,
+    userExists
 }
 
-export class User implements IUser {
+export class User {
     public userId?: number;
     public username: string = "";
     public passwordHash: string = "";
+    public set password(val: string) {
+        this.passwordHash = getHash(val);
+    }
     public role: string = "";
 
-    constructor(initialValues: IUser) {
-        this.userId = initialValues.userId;
-        this.username = initialValues.username;
-        this.passwordHash = initialValues.passwordHash;
-        this.role = initialValues.role;
+    constructor() {
+    }
+
+    private static FromRow(row: any[]): User {
+        let result = new User();
+        result.userId = row[0],
+        result.username = row[1],
+        result.passwordHash = row[2],
+        result.role = row[3]
+        return result;
     }
 
     public static async Find(userId: number): Promise<User | undefined> {
         let client = await pool.connect();
         try {
             const q = {
-                text: 'SELECT user_id, user_name, pass_hash, role FROM users WHERE user_id = $1',
+                text: 'SELECT user_id, username, pass_hash, role FROM users WHERE user_id = $1;',
                 values: [userId]
             }
             let result = await client.query(q)
-            await client.release();
             if (result.rowCount == 0) {
                 return undefined;
             }
             else {
-                return new User({
-                    userId: result.rows[0][0],
-                    username: result.rows[0][1],
-                    passwordHash: result.rows[0][2],
-                    role: result.rows[0][3]
-                });
+                return this.FromRow(result.rows[0]);
             }
         }
         finally {
             client.release();
         }
+    }
+
+    public static async FindUsername(username: string): Promise<User | undefined> {
+        let client = await pool.connect();
+        try {
+            const q = {
+                text: 'SELECT user_id, username, pass_hash, role FROM users WHERE username = $1;',
+                values: [username]
+            }
+            let result = await client.query(q)
+            if (result.rowCount == 0) {
+                return undefined;
+            }
+            else {
+                return this.FromRow(result.rows[0]);
+            }
+        }
+        finally {
+            client.release();
+        }
+    }
+
+    public async Save(): Promise<void> {
+        let client = await pool.connect();
+        try {
+            if (this.userId == undefined) {
+                const select = {
+                    text: 'INSERT INTO users (username, pass_hash, role) VALUES ($1, $2, $3) RETURNING user_id;',
+                    values: [this.username, this.passwordHash, this.role]
+                }
+                let result = await client.query(select);
+                await client.release();
+                this.userId = result.rows[0][0];
+            } else {
+                const update = {
+                    text: 'UPDATE user SET username = $1, pass_hash = $2, role = $3 WHERE user_id = $4',
+                    values: [this.username, this.passwordHash, this.role, this.userId]
+                }
+                await client.query(update);
+            }
+        }
+        finally {
+            client.release;
+        }
+    }
+
+    public static async CreateUser(username: string, password: string): Promise<UserCreateResult> {
+        if (!passwordOk(password)) {
+            return UserCreateResult.badPassword;
+        }
+        let client = await pool.connect();
+        try {
+            const q = {
+                text: 'SELECT count(user_id) FROM users WHERE username = $1;',
+                values: [username]
+            }
+            let result = await client.query(q)
+            if (result.rows[0][0] > 0) {
+                return UserCreateResult.userExists;
+            }
+        }
+        finally {
+            client.release();
+        }
+        let user = new User();
+        user.username = username;
+        user.password = password;
+        user.role = "p";
+        await user.Save();
+        return UserCreateResult.success;
     }
 }
--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,21 @@
-const { Server } = require('boardgame.io/server')
-//const { TicTacToe } = require('../game/game')
-import { PostgresStore } from "bgio-postgres"
-import { Server as ServerTypes, Game } from 'boardgame.io';
+import { Server } from 'boardgame.io/server';
+import { PostgresStore } from "bgio-postgres";
 import * as Koa from 'koa';
 import * as KoaRouter from 'koa-router';
+import session, * as KoaSession from 'koa-session';
 import koaBody from 'koa-body';
 import * as Handlebars from 'handlebars';
 import games from '../games/games';
 import * as Fs from 'fs/promises';
+import { Session as DbSession } from './db/session'
+import { User, UserCreateResult } from './db/user'
 
 const db = new PostgresStore(process.env['DB']!);
 const PORT = 2230;
 const server = Server({ games: games, db })
+const COOKIE_KEY = process.env['COOKIE_KEY'] ?? "koa.session"
+const APP_KEY = process.env['APP_KEY'] ?? "veryverysecret"
+const DEVELOPMENT = process.env['NODE_ENV'] == "development";
 
 main();
 
@@ -29,17 +33,27 @@ async function registerEndpoints() {
 
     var newuserCompiled = Handlebars.compile((await Fs.readFile("templates/newuser.hbs")).toString());
     router.get("/newuser", async (ctx: Koa.Context) => {
-        ctx.body = newuserCompiled({ name: ctx.request.ip, loggedin: false });
+        ctx.body = newuserCompiled({ name: ctx.session!.username || "No idea who ", loggedin: false });
     });
 
     router.put("/register_user", koaBody(), putRegister);
 }
 
 async function putRegister(ctx: Koa.Context) {
-    var body = ctx.request.body;
-    console.log(body);
-    ctx.status = 200;
-    ctx.body = "Fart";
+    var body = JSON.parse(ctx.request.body);
+    let result = await User.CreateUser(body.username, body.password);
+    switch (result) {
+        case UserCreateResult.badPassword:
+            ctx.response.status = 400;
+            ctx.response.body = "Invalid password - must be > 8 characters";
+            break;
+        case UserCreateResult.success:
+            ctx.response.status = 200;
+            break;
+        case UserCreateResult.userExists:
+            ctx.response.status = 400;
+            ctx.response.body = "User already exists"
+    }
 }
 
 async function registerPartials() {
@@ -47,9 +61,27 @@ async function registerPartials() {
 }
 
 function main() {
+    if (DEVELOPMENT) {
+        console.log("Detected development mode");
+    }
+    let session_config = {
+        key: COOKIE_KEY,
+        maxAge: 86400000,
+        autoCommit: true, /** (boolean) automatically commit headers (default true) */
+        overwrite: true, /** (boolean) can overwrite or not (default true) */
+        httpOnly: true, /** (boolean) httpOnly or not (default true) */
+        signed: true, /** (boolean) signed or not (default true) */
+        rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+        renew: true, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+        secure: !DEVELOPMENT, /** (boolean) secure cookie*/
+        store: new DbSession()
+    }
+    server.app.keys = [APP_KEY];
+    server.app.use(session(session_config, server.app));
+
     registerPartials().then(() => {
         registerEndpoints()
     })
-        .then(server.run(PORT))
+        .then(() => server.run(PORT))
         .catch((e) => console.log(e));
 }
