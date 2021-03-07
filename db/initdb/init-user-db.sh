b08f1fa6c95592406805e6e85a19c4d142debf59
Lachlan Kingsford <lachlan@nerdygentleman.com>
2021-03-07 03:42:49 +0000

    Get Credentials API endpoint (and auth from it)

--- db/initdb/init-user-db.sh
+++ db/initdb/init-user-db.sh
@@ -2,12 +2,19 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE users (
-        user_id SERIAL,
-        username TEXT,
-        pass_hash TEXT,
-        role TEXT,
-    );
+    CREATE TABLE public.users
+    (
+        user_id integer NOT NULL DEFAULT nextval('users_user_id_seq'::regclass),
+        username text COLLATE pg_catalog."default",
+        pass_hash text COLLATE pg_catalog."default",
+        role text COLLATE pg_catalog."default",
+        CONSTRAINT pk PRIMARY KEY (user_id)
+    )
+
+    TABLESPACE pg_default;
+
+    ALTER TABLE public.users
+        OWNER to train;
 
     CREATE TABLE public.session
     (
@@ -15,7 +22,20 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         data json NOT NULL,
         expires timestamp with time zone NOT NULL,
         CONSTRAINT session_pkey PRIMARY KEY (key)
-    )
+    );
+
+    CREATE TABLE public.credstore
+    (
+        credential character(21) COLLATE pg_catalog."default" NOT NULL,
+        "userId" integer NOT NULL,
+        created date NOT NULL,
+        CONSTRAINT credstore_pkey PRIMARY KEY (credential),
+        CONSTRAINT fk_credstore_users FOREIGN KEY ("userId")
+            REFERENCES public.users (user_id) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE CASCADE
+            NOT VALID
+    );
 
 
 EOSQL
