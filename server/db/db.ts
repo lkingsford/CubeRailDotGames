9d451e2767b16b69e90d913cb1e0bda622d0f801
Lachlan Kingsford <lachlan@nerdygentleman.com>
2022-05-21 16:22:33 +0000

    Upgrade BGIO

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,4 +1,4 @@
-import {Pool as pgPool, Client, PoolClient} from "pg";
+import { Pool as pgPool, Client, PoolClient } from "pg";
 import { promisify } from 'util';
 
 var _pool: pgPool | undefined;
@@ -7,14 +7,13 @@ var canConnect: boolean = false;
 const sleep = promisify(setTimeout);
 
 export async function Pool(): Promise<pgPool> {
-    while (!_pool || !canConnect)
-    {
+    while (!_pool || !canConnect) {
         try {
-            _pool = new pgPool({connectionString: process.env['DB']});
-            const q = { text: 'SELECT 1;'}
+            _pool = new pgPool({ connectionString: process.env['DB'] });
+            const q = { text: 'SELECT 1;' }
             let client: PoolClient | undefined;
             try {
-                client= await _pool.connect();
+                client = await _pool.connect();
                 await client.query(q);
                 canConnect = true;
             }
@@ -23,6 +22,7 @@ export async function Pool(): Promise<pgPool> {
             }
         }
         catch (ex) {
+            console.error(ex)
             console.error("Failed to connect to pg - retry in 500ms");
             await sleep(500);
         }
