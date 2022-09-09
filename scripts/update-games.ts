9d31e8a0e795a3c145fce8bad9d0e7e9521ff897
Lachlan Kingsford <lachlan@nerdygentleman.com>
2022-09-09 13:20:28 +0000

    Update EmuBay and UpdateGames

--- scripts/update-games.ts
+++ scripts/update-games.ts
@@ -1,23 +1,30 @@
-import { Clone, Repository, Oid, Reference, Revparse, Checkout, Commit } from "nodegit";
+import {
+    Clone,
+    Repository,
+    Oid,
+    Reference,
+    Revparse,
+    Checkout,
+    Commit,
+} from "nodegit";
 import * as fs from "fs";
-import * as path from 'path';
+import * as path from "path";
 import { IGameDefinition } from "../server/IGameDefinition";
-import * as child from 'child_process';
+import * as child from "child_process";
 import * as fsx from "fs-extra";
-import { rmdir } from "fs/promises";
 
-const games: IGameDefinition[] = require("../games.json")
+const games: IGameDefinition[] = require("../games.json");
 
-const gamerepos = "gamerepos"
-const gamefiles = "games"
-const clientstatic = "clients"
+const gamerepos = "gamerepos";
+const gamefiles = "games";
+const clientstatic = "clients";
 
 async function updateRepo(game: IGameDefinition) {
     console.log("Updating repo for %s", game.gameid);
     var target = path.join(gamerepos, game.gameid);
     var justCloned: boolean = false;
     if (!fs.existsSync(target)) {
-        // New - so clone 
+        // New - so clone
         console.log("Cloning %s", game.gameid);
         await Clone.clone(game.repository, target, {});
         justCloned = true;
@@ -33,25 +40,35 @@ async function updateRepo(game: IGameDefinition) {
     var commit = await Commit.lookup(repo, object.id());
     await Checkout.tree(repo, commit, { checkoutStrategy: 2 });
     await repo.setHeadDetached(commit.id());
-    console.log("%s repo is ready", game.gameid)
+    console.log("%s repo is ready", game.gameid);
 }
 
 async function extractGameTs(game: IGameDefinition) {
     console.log("Getting game.ts for %s", game.gameid);
     return new Promise<void>(function (resolve) {
-        fs.copyFile(path.join(gamerepos, game.gameid, "game", "game.ts"),
+        fs.copyFile(
+            path.join(gamerepos, game.gameid, "game", "game.ts"),
             path.join(gamefiles, `${game.gameid}.ts`),
-            () => { resolve() });
+            () => {
+                resolve();
+            }
+        );
     });
 }
 
 async function generateGameslistTs(games: IGameDefinition[]) {
-    console.log("Generating games.ts")
+    console.log("Generating games.ts");
     var output: string = "";
-    output += games.map((i,idx) => `import { ${i.object} as ${i.object}_${idx} } from "./${i.gameid}"`)
-        .join("\n")
+    output += games
+        .map(
+            (i, idx) =>
+                `import { ${i.object} as ${i.object}_${idx} } from "./${i.gameid}"`
+        )
+        .join("\n");
     output += "\n";
-    output += `const games = [${games.map((i, idx) => `${i.object}_${idx}`).join(', ')}];\n`
+    output += `const games = [${games
+        .map((i, idx) => `${i.object}_${idx}`)
+        .join(", ")}];\n`;
     output += `export default games`;
     await fsx.writeFile(path.join(gamefiles, "games.ts"), output);
 }
@@ -59,29 +76,61 @@ async function generateGameslistTs(games: IGameDefinition[]) {
 async function installClientPackages(game: IGameDefinition) {
     console.log("Installing client packages for %s", game.gameid);
     await new Promise<void>(function (resolve) {
-        var npm = child.spawn("npm", ["--prefix", path.join(gamerepos, game.gameid), "install"]);
-        npm.stdout.on("data", data => { console.log("[%s]  %s", game.gameid, data) });
-        npm.stderr.on("data", data => { console.log("[%s]! %s", game.gameid, data) });
-        npm.on("close", code => { resolve(); })
+        var npm = child.spawn("npm", [
+            "--prefix",
+            path.join(gamerepos, game.gameid),
+            "install",
+        ]);
+        npm.stdout.on("data", (data) => {
+            console.log("[%s]  %s", game.gameid, data);
+        });
+        npm.stderr.on("data", (data) => {
+            console.log("[%s]! %s", game.gameid, data);
+        });
+        npm.on("close", (code) => {
+            resolve();
+        });
     });
 }
 
 async function compileClient(game: IGameDefinition) {
     console.log("Compiling client packages for %s", game.gameid);
     await new Promise<void>(function (resolve) {
-        var env = {... process.env};
-        env['NODE_ENV'] = 'production';
-        var npm = child.spawn("npm", ["--prefix", path.join(gamerepos, game.gameid), "run-script", "build-prod"], {env: env});
-        npm.stdout.on("data", data => { console.log("[%s]  %s", game.gameid, data) });
-        npm.stderr.on("data", data => { console.log("[%s]! %s", game.gameid, data) });
-        npm.on("close", code => { resolve(); })
+        var env = { ...process.env };
+        env["NODE_ENV"] = "production";
+        var npm = child.spawn(
+            "npm",
+            [
+                "--prefix",
+                path.join(gamerepos, game.gameid),
+                "run-script",
+                "build-prod",
+            ],
+            { env: env }
+        );
+        npm.stdout.on("data", (data) => {
+            console.log("[%s]  %s", game.gameid, data);
+        });
+        npm.stderr.on("data", (data) => {
+            console.log("[%s]! %s", game.gameid, data);
+        });
+        npm.on("close", (code) => {
+            resolve();
+        });
     });
 }
 
 async function extractClientDist(game: IGameDefinition) {
-    await rmdir(path.join(clientstatic, game.gameid), { recursive: true});
+    let clientPath = path.join(clientstatic, game.gameid);
+    if (fsx.existsSync(clientPath)) {
+        await fsx.rm(clientPath, { recursive: true });
+    }
     console.log("Copying distribution for %s", game.gameid);
-    await fsx.copy(path.join(gamerepos, game.gameid, "dist-client"), path.join(clientstatic, game.gameid), { recursive: true });
+    await fsx.copy(
+        path.join(gamerepos, game.gameid, "dist-client"),
+        path.join(clientstatic, game.gameid),
+        { recursive: true }
+    );
 }
 
 async function main() {
@@ -89,14 +138,22 @@ async function main() {
     await fsx.mkdir(gamefiles, { recursive: true });
     await fsx.mkdir(clientstatic, { recursive: true });
 
-    await Promise.all(games.map(async (i) => {
-        await updateRepo(i);
-        await installClientPackages(i);
-        await compileClient(i);
-        await extractGameTs(i);
-        await extractClientDist(i);
-    }));
+    await Promise.all(
+        games.map(async (i) => {
+            await updateRepo(i);
+            await installClientPackages(i);
+            await compileClient(i);
+            await extractGameTs(i);
+            await extractClientDist(i);
+        })
+    );
     await generateGameslistTs(games);
 }
 
-main().then(() => { console.log("done") }).catch((e) => { console.log("Errored {e}", e) });
+main()
+    .then(() => {
+        console.log("done");
+    })
+    .catch((e) => {
+        console.log("Errored {e}", e);
+    });
